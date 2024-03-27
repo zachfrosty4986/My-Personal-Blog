90bdd987f83271f8fd750fb74595dd5be3eef9cb
zachfrosty4986 <zachjames0114@gmail.com>
2024-03-27 18:01:03 -0400

    Added CSS content and some JavaScript elements, Added semantic HTML elements

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,6 @@
 // ADD COMMENTS
+const username = document.getElementById("username");
+const title = document.getElementById("title");
+const context = document.getElementById("content");
+const submit = document.getElementById("submit");
+
