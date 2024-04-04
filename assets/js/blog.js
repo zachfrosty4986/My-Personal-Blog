5110c3f8f9d844fdeaf1787bfc0418871ec2db4f
zachfrosty4986 <zachjames0114@gmail.com>
2024-04-04 19:31:51 -0400

    Added Transition elements to CSS and added light/dark mode features to form.js/css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,30 @@
 //ADD COMMENTS
 // const backButton = $('#back-btn');
+const modeToggle = document.getElementById('darkLight');
 
 const posts = JSON.parse(localStorage.getItem('storedBlog'));
 console.log(posts);
 
 function name(params) {
     
+}
+
+
+
+
+
+
+
+
+
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    // Save user preference to localStorage
+    localStorage.setItem('mode', body.classList.contains('dark-mode') ? 'dark' : 'light');
+});
+        
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode === 'dark') {
+    body.classList.add('dark-mode')
 }
