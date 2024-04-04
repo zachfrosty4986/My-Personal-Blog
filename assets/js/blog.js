ff992c1effdcc404550998fec29f36afbd81d570
zachfrosty4986 <zachjames0114@gmail.com>
2024-04-04 19:49:41 -0400

    added content to blog.js, fixed transitions in CSS

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,18 +5,34 @@ const modeToggle = document.getElementById('darkLight');
 const posts = JSON.parse(localStorage.getItem('storedBlog'));
 console.log(posts);
 
-function name(params) {
+function addBlog(postBlog) {
+    let blogObject = JSON.parse(postBlog)
+    console.log(blogObject);
+
+    const container = document.querySelector('.container')
+    const contentDiv = document.createElement('div')
+    const newUser = document.createElement('h2')
+    const newtitle = document.createElement('h3')
+    const newBlog = document.createElement('p')
+
+
+    contentDiv.classlist.add('card')
+    newUser.classlist.add('username')
+    newtitle.classlist.add('title')
+    newBlog.classlist.add('content')
+
     
+    newUser.textContent = ('${blogObject.username}')
+    newtitle = ('${blogObject.title}')
+    newBlog = ('${blogObject.content}')
+
+
+    container.appendChild(contentDiv)
+    contentDiv.appendChild(newUser)
+    contentDiv.appendChild(newTitle)
+    contentDiv.appendChild(newBlog)
+
 }
-
-
-
-
-
-
-
-
-
 
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
