62c4999e86ef9ab75846329932bb43ecf2a29adc
zachfrosty4986 <zachjames0114@gmail.com>
2024-04-05 14:45:00 -0400

    Added DarkLight configuration for blog, added comments to CSS and JS

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,46 +1,52 @@
 //ADD COMMENTS
-// const backButton = $('#back-btn');
+// defining elements
 const modeToggle = document.getElementById('darkLight');
+const body = document.body;
 
 const posts = JSON.parse(localStorage.getItem('storedBlog'));
 console.log(posts);
 
 function addBlog(postBlog) {
-    let blogObject = JSON.parse(postBlog)
-    console.log(blogObject);
-
+//creating elements
     const container = document.querySelector('.container')
     const contentDiv = document.createElement('div')
     const newUser = document.createElement('h2')
-    const newtitle = document.createElement('h3')
+    const newTitle = document.createElement('h3')
     const newBlog = document.createElement('p')
 
+// adding classes
+    contentDiv.classList.add('card');
+    newUser.classList.add('username');
+    newTitle.classList.add('title');
+    newBlog.classList.add('content');
 
-    contentDiv.classlist.add('card')
-    newUser.classlist.add('username')
-    newtitle.classlist.add('title')
-    newBlog.classlist.add('content')
+    //creating the text to push
+    newUser.textContent = postBlog.username;
+    newTitle.textContent = postBlog.title;
+    newBlog.textContent = postBlog.content;
 
-    
-    newUser.textContent = ('${blogObject.username}')
-    newtitle = ('${blogObject.title}')
-    newBlog = ('${blogObject.content}')
-
-
+ //adding it to blog
     container.appendChild(contentDiv)
     contentDiv.appendChild(newUser)
     contentDiv.appendChild(newTitle)
     contentDiv.appendChild(newBlog)
 
 }
-
+// for loop for adding to webpage
+for (let index = 0; index < posts.length; index++) {
+    addBlog(posts[index])
+    
+}
+// darkLight button
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     // Save user preference to localStorage
-    localStorage.setItem('mode', body.classList.contains('dark-mode') ? 'dark' : 'light');
+    localStorage.setItem('mode', body.classList.contains('darkLight') ? 'dark' : 'light');
 });
         
     const savedMode = localStorage.getItem('mode');
     if (savedMode === 'dark') {
     body.classList.add('dark-mode')
 }
+
+
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,26 +1,26 @@
-// ADD COMMENTS
+// Defining elements
 const form = document.getElementById("form-boxes")
 const userInput = document.getElementById("username");
 const titleInput = document.getElementById("title");
-const contextInput = document.getElementById("content");
+const contentInput = document.getElementById("content");
 const modeToggle = document.getElementById('mode-toggle');
-const body = document.body
+const body = document.body;
 const submitPost = document.getElementById("submit");
 
 console.log(form)
-
+//submit functionality
 form.addEventListener('submit', function(event) {
     console.log("Submitted sucsessfully!")
     event.preventDefault();
-
-    const posts = [];
-
+// adding content to local storage
+    const posts = JSON.parse(localStorage.getItem('storedBlog')) || [];
+// form box configuration
     const blog = {
         username: userInput.value.trim(),
         title: titleInput.value.trim(),
-        context: contextInput.value.trim(),
+        content: contentInput.value.trim(),
     }
-
+// if form is not complete, message appears for what needs to be completed
     if (userInput.value === "") {
         confirm("Please enter a Username")
         return
@@ -31,11 +31,12 @@ form.addEventListener('submit', function(event) {
         return
     }
 
-    if (contextInput.value === "") {
+    if (contentInput.value === "") {
         confirm("Please add your Context")
         return
     }
 
+// pushes box content to local storage 
     posts.push(blog)
     let localBlog = JSON.stringify(posts)
     localStorage.setItem('storedBlog', localBlog)
@@ -43,7 +44,7 @@ form.addEventListener('submit', function(event) {
     console.log(localBlog)
     document.location.replace('./blog.html')
 });
-
+// darkLight button
     modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     // Save user preference to localStorage
